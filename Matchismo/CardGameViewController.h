d80fcccc667e55dc90cf6ad7574524c9a4b2d9a8
Beau Romero <beau@identitymad.com>
2013-01-27 16:55:26 -0600

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Beau Romero on 1/27/13.
+//  Copyright (c) 2013 Identity Mad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
